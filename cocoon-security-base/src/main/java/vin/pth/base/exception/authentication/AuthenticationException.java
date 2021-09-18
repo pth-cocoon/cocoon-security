57aee09eecdee1e3c7a36491e7adfb6ac93739cc
Cocoon <6631097@qq.com>
2021-09-18 18:43:47 +0800

    完善异常

--- cocoon-security-base/src/main/java/vin/pth/base/exception/authentication/AuthenticationException.java
+++ cocoon-security-base/src/main/java/vin/pth/base/exception/authentication/AuthenticationException.java
@@ -1,11 +1,19 @@
 package vin.pth.base.exception.authentication;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 鉴权异常
  */
+@Getter
+@Setter
 public class AuthenticationException extends RuntimeException {
+
+  private String message;
 
   public AuthenticationException(String message) {
     super(message);
+    this.message = message;
   }
 }
--- cocoon-security-base/src/main/java/vin/pth/base/exception/authorization/AuthorizationException.java
+++ cocoon-security-base/src/main/java/vin/pth/base/exception/authorization/AuthorizationException.java
@@ -1,8 +1,19 @@
 package vin.pth.base.exception.authorization;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 授权异常
  */
+@Getter
+@Setter
 public class AuthorizationException extends RuntimeException {
 
+  private String message;
+
+  public AuthorizationException(String message) {
+    super(message);
+    this.message = message;
+  }
 }
