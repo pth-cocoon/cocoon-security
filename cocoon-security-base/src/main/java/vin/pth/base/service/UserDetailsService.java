37ac396eb0561e66e89a4a322b0a00ec66a415d8
cocoon <6631097@qq.com>
2021-09-26 23:37:29 +0800

    完善UserDetailsService异常说明

--- cocoon-security-base/src/main/java/vin/pth/base/service/UserDetailsService.java
+++ cocoon-security-base/src/main/java/vin/pth/base/service/UserDetailsService.java
@@ -1,13 +1,35 @@
 package vin.pth.base.service;
 
 
+import vin.pth.base.exception.authentication.AuthenticationException;
+import vin.pth.base.exception.authorization.AuthorizationException;
 import vin.pth.base.pojo.UserDetails;
 
 public interface UserDetailsService {
 
+  /**
+   * 通过用户名获取用户信息
+   *
+   * @param username 用户名
+   * @return 用户信息
+   */
   UserDetails loadUserByUsername(String username);
 
-  UserDetails getByToken(String token);
+  /**
+   * 通过Token获取用户信息
+   *
+   * @param token 令牌
+   * @return 用户信息
+   * @throws AuthenticationException 鉴权异常
+   */
+  UserDetails getByToken(String token) throws AuthenticationException;
 
-  String getToken(UserDetails userDetails);
+  /**
+   * 生成Token
+   *
+   * @param userDetails 用户信息
+   * @return 生成的Token
+   * @throws AuthorizationException 授权异常
+   */
+  String generateToken(UserDetails userDetails) throws AuthorizationException;
 }
